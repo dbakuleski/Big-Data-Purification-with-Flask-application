f50e5ec0ce5a7b497b8797192d85912681b4b4a2
dbakuleski <bakuleskid@gmail.com>
2023-01-31 03:00:33 +0100

    Cleaning the names from unwanted characters.

--- names.py
+++ names.py
@@ -1,34 +1,15 @@
-from flask import Flask, request
-import sqlite3
-import random
 import re
+import requests
 
-app = Flask(__name__)
-connect = sqlite3.connect(r"/home/visitor/Desktop/Realen-Proekt-Python/data.db")
-cursor = connect.cursor()
+response = requests.get("http://127.0.0.1:5350/")
+oneCompany = response.json()
+print(oneCompany)
 
-
-@app.route("/one_company", methods=["GET"])
-def readOneCompany():
-    sql = "select * from companies"
-    cursor.execute(sql)
-    results = cursor.fetchone()
-    return results
-
-
-query = "select name from companies"
-names = cursor.execute(query).fetchall()
 
 cleaned_names = []
-for name in names:
+for name in oneCompany:
     name = name.strip()
     name = name.replace(" ", "")
     name = re.sub(r"[^a-zA-Z0-9]+", "", name)
     cleaned_names.append(name)
 
-if __name__ == '__main__':
-    port = 5000 + random.randint(0, 999)
-    print(port)
-    url = "http://127.0.0.1:{0}".format(port)
-    print(url)
-    app.run(use_reloader=False, debug=True, port=port)
