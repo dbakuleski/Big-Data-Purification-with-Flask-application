3e2e0cebace67c026c29877855e4e52dbfbab158
dbakuleski <bakuleskid@gmail.com>
2023-02-02 22:05:12 +0100

    Cleaning the company names.

--- names.py
+++ names.py
@@ -1,20 +1,25 @@
 import re
 import requests
-import json
+from cleanco import basename
+import string
 
-response = requests.get("http://127.0.0.1:5964/")
-oneCompany = response.json()
-print(oneCompany)
+response = requests.get("http://127.0.0.1:5964/one_company")
+data = response.json()
+company_name = data[" "]
 
 
-cleaned_names = []
-for name in oneCompany:
+def clean_company_name(name):
     name = name.strip()
     name = name.replace(" ", "")
     name = re.sub(r"[^a-zA-Z0-9]+", "", name)
-    cleaned_names.append(name)
+    return name
 
 
+cleaned_company_name = clean_company_name(company_name)
+cleaned_company_name = basename(cleaned_company_name)
+cleaned_company_name = basename(cleaned_company_name)
 
 
-
+def normal_company_name():
+    normal_name = string.capwords(cleaned_company_name)
+    return normal_name
